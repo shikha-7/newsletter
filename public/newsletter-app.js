d6b345d64ec4b5bdcddb6cd01157c21e5b248208
shikha <shikhakumari522@gmail.com>
2022-07-30 17:08:07 +0530

    changes in fronted

--- public/newsletter-app.js
+++ public/newsletter-app.js
@@ -2,7 +2,7 @@
 const formDOM = document.querySelector('.form');
 const nameDOM = document.querySelector('#name');
 const emailDOM = document.querySelector('#email');
-const passwordDOM = document.querySelector('#password');
+// const passwordDOM = document.querySelector('#password');
 const formAlertDOM = document.querySelector('.form-alert');
 const formInputDOM = document.querySelectorAll('.form-input');
 const btnDOM = document.querySelector('.btn');
@@ -14,9 +14,9 @@ formDOM.addEventListener("submit", async (e) => {
 
     const name = nameDOM.value;
     const email = emailDOM.value;
-    const password = passwordDOM.value;
+    // const password = passwordDOM.value;
     try {
-        const { data } = await axios.post("/api/v1/user/register", { name, email, password });
+        const { data } = await axios.post("/api/v1/user/register", { name, email });
         e.target.reset();
         formAlertDOM.style.display = 'block'
         formAlertDOM.textContent = `successfully logged in`;
